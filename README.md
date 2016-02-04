3d4869be165e8e994a699269e23e3ed2f89d887e
Ted Chen <ted.chen@emc.com>
2016-02-04 00:04:43 -0500

    The initial workable version

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # hash-gen
+
+This is a tool for generating hash and salt for monorail.json configure file
+that is used by RackHD on-http project.
+
+Link to [monorail.json](https://github.com/RackHD/RackHD/blob/master/packer%2Fansible%2Froles%2Fmonorail%2Ffiles%2Fmonorail.json)
+
+## Output from the project
+
+Giving that the password you wanted to use for RackHD is PASSWORD, you will
+get following output from this project, that are configuration items needed
+in following monorail.json:
+
+* authPasswordSalt: The salt used to generate the password hash. It's base64
+coded. It's better to be a random value.
+* authPasswordHash: The hash generated from PASSWORD and authPasswordSalt
+using crypto.pbkdf2. It's base 64 coded.
+
+
+## User preference input to the project
+
+* password: the user password that needs to generate hash from
+* generateRandomSalt: specify whether users want to generate a random salt
+using this project, or they prefer to use salt defined by themselves.
+    * true: Need a generated random salt. This is the default value.
+    * false: user want to use their own salt.
+* userSalt: specify a non-empty string value as a salt to generate the hash,
+if generateRandomSalt is set false.
---
+++ hash-gen.js
@@ -0,0 +1,62 @@
+var crypto = require('crypto');
+var Promise = require('bluebird');
+
+// User inputs
+var password = 'admin123';      // Put user password here
+var generateRandomSalt = false;
+var userSalt = 'zlxkgxjvcFwm0M8sWaGojh25qNYO8tuNWUMN4xKPH93PidwkCAvaX2JItLA3p7BSCWIzkw4GwWuezoMvKf3UXg==';
+
+
+var hashConfig = {  //needs to be the same as RackHD. Do not change this
+    // number of bytes in the Hash
+    hashBytes: 64,
+    // number of bytes in the slat
+    saltBytes: 64,
+    // number of iterations to get the final hash, longer means more secure,
+    // but also slower
+    iterations: 10000
+};
+
+var encoder = 'base64';
+
+var calcPasswordHash = function (password, salt, iteration, bytes, callback) {
+    crypto.pbkdf2(password, salt, hashConfig.iterations, hashConfig.hashBytes,
+        function(err, hash){
+            if(err){
+                callback(err);
+            }
+            callback(null, hash);
+        });
+};
+
+var getSalt = function (){
+    return new Promise(function(resolve, reject){
+        debugger;
+
+        if(generateRandomSalt){
+            crypto.randomBytes(hashConfig.saltBytes, function(err, randomSalt){
+                if (err){
+                    reject(err);
+                }
+                resolve(randomSalt);
+            });
+        }
+        else {
+            resolve(new Buffer(userSalt, encoder));
+        }
+    });
+};
+
+// The main process
+
+getSalt().then(function(salt){
+    calcPasswordHash(password, salt, hashConfig.iterations, hashConfig.hashBytes, function(err, hash) {
+        if (err){
+            console.log('error', err);
+        }
+        else{
+            console.log('salt = ', salt.toString(encoder));
+            console.log('hash = ', hash.toString(encoder));
+        }
+    })
+});
